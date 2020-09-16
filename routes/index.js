d27f96db9220b8537c837127e932c4315254bd12
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-16 15:30:20 +1200

    refactor routes file

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,15 @@
 const express = require('express');
+const { XML, XMLOrSitemap, AllSiteMaps } = require('../public/javascripts/utils');
 const router = express.Router();
-const request = require('superagent')
-const fn = require('../public/javascripts/xml2links')
+
 
 /* GET home page. */
-router.get('/xml/:url', (req, res, next) => {
-  request.get(`${fn.validateUrl(req.params.url)}robots.txt`)
-    .then(roboData => fn.getSitemapLink(roboData, filteredData => {
-      if(filteredData.length === 1){
-        console.log(filteredData)
-        request.get(filteredData[0])
-          .then(xml => fn.dataFilter(xml, filteredData[0], links => {
-            res.status(200).json({links: links})
-          }))
-      }else{
-        res.status(200).json({ sitemaps: filteredData })
-      }
-    }))
-    .catch(err => {
-      console.log('Error\n\n\n\nBad request',err)
-      res.status(err.status).json({err: err.response})
-    })
-});
+//Get XML sitemaps links only
+router.get('/xml', XML)
+//If there is 1 link auto expand it
+//otherwise same as 1st route
+router.get('/xml/auto', XMLOrSitemap)
+//Open all xml links and construct and object with all the data
+router.get('/xml/auto/full', AllSiteMaps)
 
 module.exports = router;
