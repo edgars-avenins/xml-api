0a833c9854e0b9509021fb31708bf268cec3a706
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-21 11:05:11 +1200

    formatting

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,68 +1,69 @@
 const request = require('superagent')
 const fn = require('./xml2links')
 
-function XML(req, res){
-    const validUrl = fn.validateUrl(req.body.url)
+function XML(req, res) {
+  const validUrl = fn.validateUrl(req.body.url)
 
-    request.get(`${validUrl}robots.txt`)
-        .then(xml => fn.getSitemapLink(xml, (err, links) => {
-            if(err) res.status(400).json(err)
-            res.status(200).json({links})
-        }))
-        .catch()
+  request.get(`${validUrl}robots.txt`)
+    .then(xml => fn.getSitemapLink(xml, (err, links) => {
+      if (err) res.status(400).json(err)
+      res.status(200).json({ links })
+    }))
+    .catch()
 }
 
 function XMLOrSitemap(req, res) {
-    const validUrl = fn.validateUrl(req.body.url)
+  const validUrl = fn.validateUrl(req.body.url)
 
-    request.get(`${validUrl}robots.txt`)
+  request.get(`${validUrl}robots.txt`)
     .then(roboData => fn.getSitemapLink(roboData, (err, filteredData) => {
-        if(err) res.status(400).json(err)
+      if (err) res.status(400).json(err)
 
-        if(filteredData.length === 1){
-          recOpenSitemaps(filteredData, res)
-        }else if(filteredData.length === 0){
-            res.status(301).json({text: 'no sitemaps found...'})
+      if (filteredData.length === 1) {
+        recOpenSitemaps(filteredData, res)
+      } else if (req.params.full === 'full' && filteredData.length > 1) {
+        recOpenAllSitemaps(filteredData, res)
+      } else if (filteredData.length === 0) {
+        res.status(301).json({ text: 'no sitemaps found...' })
 
-        }else{
-            res.status(200).json({ sitemaps: filteredData })
+      } else {
+        res.status(200).json({ sitemaps: filteredData })
       }
     }))
     .catch(() => {
       request.get(`${validUrl}sitemap.xml`)
         .then(data => fn.dataFilter(data, filteredData[0], (err, links) => {
-          res.status(200).json({links: links})
+          res.status(200).json({ links: links })
         }))
         .catch(err => {
-          console.log('Error\n\n\n\nBad request',err)
-          res.status(err.status).json({err: err.response})
+          console.log('Error\n\n\n\nBad request', err)
+          res.status(err.status).json({ err: err.response })
         })
     })
 }
 
-function AllSiteMaps(req, res) {
-    const validUrl = fn.validateUrl(req.body.url)
-    
+function recOpenAllSitemaps(req, res) {
+  const validUrl = fn.validateUrl(req.body.url)
+
 
 }
 
 function recOpenSitemaps(data, res, sitemap = [data[0]]) {
   request.get(data[0])
     .then(xml => fn.dataFilter(xml, data[0], (err, links) => {
-    //refactor into recursive function ???
-      if(links.isXML){
+      //refactor into recursive function ???
+      if (links.isXML) {
         sitemap.push(links.dataArray[0])
         recOpenSitemaps(links.dataArray, res, sitemap)
-      }else{
+      } else {
         links.sitemapTree = sitemap
-        res.status(200).json({links: links})
+        res.status(200).json({ links: links })
       }
     }))
 }
 
 
 module.exports = {
-    XML,
-    XMLOrSitemap,
-    AllSiteMaps
+  XML,
+  XMLOrSitemap
 }
