9fee29b3b50ff55086f540ceecae7efe343418d2
Edgars Avenins <edgars.avenins@gmail.com>
2020-09-21 17:19:09 +1200

    add a link for direct sitemap urls

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { XML, XMLOrSitemap } = require('../public/javascripts/utils');
+const { XML, XMLOrSitemap, sitemap } = require('../public/javascripts/utils');
 const router = express.Router();
 
 
 /* GET home page. */
 //Get XML sitemaps links only
 router.get('/xml', XML)
+//Get sitemap links from a provided xml link
+router.get('/sitemap', sitemap)
 //If there is 1 link auto expand it
 //otherwise same as 1st route
 router.get('/xml/auto', XMLOrSitemap)
